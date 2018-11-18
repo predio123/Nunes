ad56ea35e87986c84530f24795a96e8e60d6be8d
predio123 <jpsantos.1995@sapo.pt>
2018-11-18 01:15:55 +0000

    Introdução do modo de telemóvel
    Mexida com os comandos de modo a ter uma versão de telemóvel usável.

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,6 +3,7 @@ var items = 0;
 var lista = [];
 var vendas = 0;
 var nega = true;
+var state = 0;
 
 /** New Match object Creator**/
 function product(name, price) {
@@ -10,6 +11,7 @@ function product(name, price) {
     this.preco = price;
     this.acumulado = 0;
     this.total = 0;
+    this.stock = 0;
 }
 
 function dEntrada() {
@@ -37,13 +39,70 @@ function dEntrada() {
 }
 
 function tablet() {
+    for (i = 1; i < (items + 1); i++) {
+        aux = '#nome' + (i).toString();
+        noy = $(aux).val();
+        lista[i - 1].nome = noy;
+        aux = '#preco' + (i).toString();
+        precy = $(aux).val();
+        lista[i - 1].preco = Number(precy);
+    }
+    // See if more items have been added
+    aux = '#nome' + (items + 1).toString();
+    noy = $(aux).val();
+    if (noy.length > 0) {
+        items = items + 1;
+        var aux = '#nome' + items.toString();
+        var noy = $(aux).val();
+        aux = '#preco' + items.toString();
+        var precy = $(aux).val();
+        var newyou = new product(noy, Number(precy));
+        lista.push(newyou);
+    }
+
     for (i in lista) {
-        var ready = "<ul><div class='item'><div class='name'>" + lista[i].nome + "   (" + lista[i].preco.toFixed(2) + "€)" + "</div><div class='plus' onclick='subir(" + i + ")'>+</div><div class='num' id='num" + i + "'>0</div><div class='minus' onclick='descer(" + i + ")'>-</div></div></ul>"
+        var ready = "<ul class='ass'><div class='item'><div class='name'>" + lista[i].nome + "   (" + lista[i].preco.toFixed(2) + "€)" + "</div><div class='plus' onclick='subir(" + i + ")'>+</div><div class='num' id='num" + i + "'>0</div><div class='minus' onclick='descer(" + i + ")'>-</div></div></ul>"
         $("#rede").append(ready);
         ready = "<div id='produ" + i.toString() + "' class='y'></div>";
         $("#resultsp").append(ready);
     }
     $('#page1').toggle();
+    $('#page3').hide();
+    state = 0;
+    upDateTotal();
+}
+
+function phone() {
+    for (i = 1; i < (items + 1); i++) {
+        aux = '#nome' + (i).toString();
+        noy = $(aux).val();
+        lista[i - 1].nome = noy;
+        aux = '#preco' + (i).toString();
+        precy = $(aux).val();
+        lista[i - 1].preco = Number(precy);
+    }
+    // See if more items have been added
+    aux = '#nome' + (items + 1).toString();
+    noy = $(aux).val();
+    if (noy.length > 0) {
+        items = items + 1;
+        var aux = '#nome' + items.toString();
+        var noy = $(aux).val();
+        aux = '#preco' + items.toString();
+        var precy = $(aux).val();
+        var newyou = new product(noy, Number(precy));
+        lista.push(newyou);
+    }
+
+    for (i in lista) {
+        var ready = "<ul class='tits' onclick='subir(" + i + ")'><div class='item1'><div class='name1'>" + lista[i].nome + "</div><div class='num1' id='num" + i + "'>0</div><div class='minus1' onclick='descer(" + i + "); event.stopPropagation(); '>-</div></div></ul>";
+        $("#rede1").append(ready);
+            ready = "<div id='produ" + i.toString() + "' class='t'></div>";
+            $("#resultsp1").append(ready);
+    }
+    $('#page1').toggle();
+    $('#page2').hide();
+    state = 1;
     upDateTotal();
 }
 
@@ -71,26 +130,53 @@ function descer(i) {
 }
 
 function upDateTotal() {
+
     total = 0;
     for (i in lista) {
         total = total + lista[i].preco * lista[i].acumulado;
     }
     var texto = total.toFixed(2) + '€';
-    $('#target').text(texto);
+    if (state == 0) {
+        $('#target').text(texto);
+    } else {
+        $('#target1').text(texto);
+    }
 }
 
 function neworder() {
+    if (state == 0) {
+        for (i in lista) {
+            lista[i].total = lista[i].total + lista[i].acumulado;
+            vendas = vendas + lista[i].acumulado * lista[i].preco;
+            lista[i].acumulado = 0;
+            aux = '#num' + i.toString();
+            $(aux).text("0");
+        }
+        $("#pago").val("");
+        $("#resto").text("");
+        upDateTotal();
+        console.log(vendas);
+    } else {
+        for (i in lista) {
+            lista[i].total = lista[i].total + lista[i].acumulado;
+            vendas = vendas + lista[i].acumulado * lista[i].preco;
+            lista[i].acumulado = 0;
+            aux = '#num' + i.toString();
+            $(aux).text("0");
+        }
+        upDateTotal();
+        console.log(vendas);
+    }
+}
+
+function cancel() {
     for (i in lista) {
-        lista[i].total = lista[i].total + lista[i].acumulado;
-        vendas = vendas + lista[i].acumulado * lista[i].preco;
         lista[i].acumulado = 0;
         aux = '#num' + i.toString();
-        $(aux).text("0");
+        $(aux).text("0");  
     }
-    $("#pago").val("");
-    $("#resto").text("");
     upDateTotal();
-    console.log(vendas);
+        console.log('Compra Cancelada...');
 }
 
 function troco() {
@@ -101,15 +187,28 @@ function troco() {
 }
 
 function resultados() {
-    var text = "Vendas: " + vendas.toFixed(2) + "€";
-    $("#ppreco").text(text);
-    for (i in lista) {
-        text = lista[i].nome + ": " + lista[i].total + " items.";
-        aux = "#produ" + i.toString();
-        $(aux).text(text);
+    if (state == 0) {
+        var text = "Vendas: " + vendas.toFixed(2) + "€";
+        $("#ppreco").text(text);
+        for (i in lista) {
+            text = lista[i].nome + ": " + lista[i].total + " items.";
+            aux = "#produ" + i.toString();
+            $(aux).text(text);
+        }
+        $("#resultsp").show();
+        $("#left-menu").hide();
+    } else {
+        var text = "Vendas: " + vendas.toFixed(2) + "€";
+        $("#ppreco1").text(text);
+        for (i in lista) {
+            text = lista[i].nome + ": " + lista[i].total + " items.";
+            aux = "#produ" + i.toString();
+            $(aux).text(text);
+        }
+        $("#resultsp1").show();
+        $("#left-menu1").hide();
     }
-    $("#resultsp").show();
-    $("#left-menu").hide();
+
 }
 
 function colorup() {
@@ -117,8 +216,14 @@ function colorup() {
         $("#rail").css({
             backgroundColor: 'green'
         });
+        $("#rail1").css({
+            backgroundColor: 'green'
+        });
     } else {
         $("#rail").css({
+            backgroundColor: 'red'
+        });
+        $("#rail1").css({
             backgroundColor: 'red'
         });
     }
@@ -130,18 +235,29 @@ function nas() {
         $("#rail").css({
             backgroundColor: 'green'
         });
+        $("#rail1").css({
+            backgroundColor: 'green'
+        });
     } else {
         nega = false;
         $("#rail").css({
             backgroundColor: 'red'
         });
+        $("#rail1").css({
+            backgroundColor: 'red'
+        });
     }
 }
-
-function evento()
-{
-    var agreg =Number($("#event").val());
-    vendas=vendas+agreg;
-    agreg =0;
-    $("#event").val(agreg);
+function evento() {
+    if (state == 0) {
+        var agreg = Number($("#event").val());
+        vendas = vendas + agreg;
+        agreg = 0;
+        $("#event").val(agreg);
+    } else {
+        var agreg = Number($("#event1").val());
+        vendas = vendas + agreg;
+        agreg = 0;
+        $("#event1").val(agreg);
+    }
 }
